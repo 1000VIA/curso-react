cd6fbeb16edd988316b018aedf622837b13f8d2d
modelnetwork2 <1000VIA>
2020-03-28 18:06:00 -0500

    pasar parametros por medio de funciones

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+const user = {
+  name: "Milvia Alvarez Zabala",
+  age: 31,
+  coutry: "Colombia"
+};
+
+const getInfo = () => `Hola ni nombre es ${user.name} y soy de Medellín`;
+
 ReactDOM.render(
-  <React.StrictMode>Hola React</React.StrictMode>,
+  <React.StrictMode>
+    <h1>{getInfo(user)}</h1>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
