db53d0d23b4e1114d0f3cb4b9f2fa967074df446
modelnetwork2 <1000VIA>
2020-03-28 20:07:41 -0500

    create component TarjetaFruta

--- src/components/tarjeta-fruta/tarjeta-fruta.jsx
+++ src/components/tarjeta-fruta/tarjeta-fruta.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from "../tarjeta-fruta/tarjeta-fruta.module.scss";
+import { FiPlusSquare } from "react-icons/fi";
 const TarjetaFruta = props => {
   return (
-    <div>
-      <h3>{props.name}</h3>
-      <hr />
-      <p>$ {props.price}</p>
+    <div className={styles.card}>
+      <div className={styles.name}>
+        <h3>{props.name}</h3>
+      </div>
+      <div className={styles.price}>
+        <p>$ {props.price}</p>
+      </div>
+      <div className={styles.button}>
+        <button>
+          Agregar <FiPlusSquare />
+        </button>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -14,9 +14,18 @@ const getInfo = () => `Hola ni nombre es ${user.name} y soy de Medellín`;
 ReactDOM.render(
   <React.StrictMode>
     <h1>{getInfo(user)}</h1>
-    <TarjetaFruta name="Sandía" price={2000} />
-    <TarjetaFruta name="Naranja" price={1500} />
-    <TarjetaFruta name="kiwi" price={3400} />
+    <div
+      style={{
+        display: "flex",
+        padding: "1rem 0",
+        overflow: "hidden",
+        flexWrap: "wrap"
+      }}
+    >
+      <TarjetaFruta name="Sandía" price={2000} />
+      <TarjetaFruta name="Naranja" price={1500} />
+      <TarjetaFruta name="kiwi" price={3400} />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
